2dfa46e7ee135d0d63f76a14a92872a85146bdb6
LeighGarabz <81326911+LeighGarabz@users.noreply.github.com>
2021-05-13 17:46:30 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:my_cert/screens/GettingStartedScreen.dart';
 import 'package:my_cert/screens/LoginScreen.dart';
-import 'nav/Nav.dart';void main() {
-  runApp(MyApp());
+
+void main(){
+  runApp (MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: GettingStarted.id,
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
       routes: {
-        LoginScreen.id :(context) =>LoginScreen(),
-        Nav.id :(context) =>Nav(),
-        GettingStarted.id :(context) =>GettingStarted()
+        'login':(context)=>LoginScreen()
       },
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  void startTimer(){
+    Timer(Duration(seconds: 7), (){
+Navigator.of(context).pushReplacementNamed('login');
+
+  });
+  }
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/images/Untitled design (2).png', width: MediaQuery.of(context).size.width*.5,),
+      )
+    );
+  }
+
+}
+
+
+
 
